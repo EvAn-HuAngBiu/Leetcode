ecfc7b7854b1abeb3a2ea8a4e24b84877098bb65
EvAn-HuAngBiu <17321168224@163.com>
2020-07-30 19:33:55 +0800

    Update question 154

--- 154.cpp
+++ 154.cpp
@@ -5,21 +5,34 @@
 using namespace std;
 
 int findMin(vector<int>& nums) {
-    int low = 0, high = nums.size() - 1, mid;
-    while (low < high) {
-        mid = low + (high - low) / 2;
-        if (nums[mid] < nums[high]) {
-            high = mid;
-        } else if (nums[mid] > nums[high]) {
-            low = mid + 1;
+    int beg = 0, end = nums.size() - 1, mid;
+    if (end == 0) {
+        return nums[0];
+    }
+    while (beg <= end) {
+        mid = (beg + end) / 2;
+        if (nums[mid] > nums[mid + 1]) {
+            return nums[mid + 1];
+        }
+        if (nums[mid] > nums[end]) {
+            beg = mid + 1;
+        } else if (nums[mid] < nums[end]) {
+            end = mid;
         } else {
-            --high;
+            --end;
         }
     }
-    return nums[low];
+    return nums[beg];
 }
 
 int main()
 {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    cout << findMin(v) << endl;
     return 0;
 }
