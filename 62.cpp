1899c3e22b4399d2ee8889bb5f688cb725d20168
EvAn-HuAngBiu <17321168224@163.com>
2020-08-10 08:55:28 +0800

    Init question 62

--- 62.cpp
+++ 62.cpp
@@ -36,7 +36,7 @@ public:
 
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m, vector<int>(n, 0));
-        dp[m - 1][n - 1] = dp[m - 1][n - 1] = 1;
+        dp[m - 1][n - 1] = 1;
         for (int i = m - 1; i >= 0; --i) {
             for (int j = n - 1; j >= 0; --j) {
                 if (i == m - 1 && j == n - 1) {
