e6ea3ff9447f87d819d212672cb7bfab59bb7f3e
EvAn-HuAngBiu <17321168224@163.com>
2020-08-11 08:38:55 +0800

    Init question 130

---
+++ 130.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+private:
+    int m, n;
+public:
+    void dfs(vector<vector<char>>& board, int x, int y) {
+        if (x >= m || x < 0 || y >= n || y < 0 || board[x][y] != 'O') {
+            return;
+        }
+        board[x][y] = 'A';
+        dfs(board, x + 1, y);
+        dfs(board, x - 1, y);
+        dfs(board, x, y + 1);
+        dfs(board, x, y - 1);
+    }
+
+    void solve(vector<vector<char>>& board) {
+        m = board.size();
+        if (!m) {
+            return;
+        }
+        n = board[0].size();
+        for (int i = 0; i < n; ++i) {
+            dfs(board, 0, i);
+            dfs(board, m - 1, i);
+        }
+        for (int i = 0; i < m; ++i) {
+            dfs(board, i, n - 1);
+            dfs(board, i, 0);
+        }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                board[i][j] = (board[i][j] == 'O' ? 'X' : (board[i][j] == 'A' ? 'O' : 'X'));
+            }
+        }
+    }
+};
+
+int main()
+{
+    return 0;
+}
