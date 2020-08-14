69e589c95c40b11b534cf41ce444111b65826065
EvAn-HuAngBiu <17321168224@163.com>
2020-08-14 08:31:55 +0800

    Update question 207

--- 207.cpp
+++ 207.cpp
@@ -5,37 +5,46 @@
 #include <queue>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
     vector<int> inner;
     vector<vector<int>> graph;
 
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    bool canFinish(int numCourses, vector<vector<int>> &prerequisites)
+    {
         inner.assign(numCourses, 0);
         graph.assign(numCourses, vector<int>());
-        for (const vector<int>& prerequisite : prerequisites) {
+        for (const vector<int> &prerequisite : prerequisites)
+        {
             int cur = prerequisite[0];
-            for (int i = 1; i < prerequisite.size(); ++i) {
+            for (int i = 1; i < prerequisite.size(); ++i)
+            {
                 graph[cur].emplace_back(prerequisite[i]);
                 ++inner[prerequisite[i]];
             }
         }
 
         queue<int> available;
-        for (int i = 0; i < numCourses; ++i) {
-            if (inner[i] == 0) {
+        for (int i = 0; i < numCourses; ++i)
+        {
+            if (inner[i] == 0)
+            {
                 available.emplace(i);
             }
         }
         int done = 0;
-        while (!available.empty()) {
+        while (!available.empty())
+        {
             int cur = available.front();
             available.pop();
             ++done;
             inner[cur] = -1;
-            for (auto& c : graph[cur]) {
+            for (auto &c : graph[cur])
+            {
                 --inner[c];
-                if (inner[c] == 0) {
+                if (inner[c] == 0)
+                {
                     available.emplace(c);
                 }
             }
@@ -46,8 +55,21 @@ public:
 
 int main()
 {
-    int numCourses;
-    vector<vector<int>> v = {{1, 0}, {0, 1}};
-    cout << boolalpha << Solution().canFinish(2, v) << endl;
+    vector<vector<int>> prerequisites;
+    int numCourses, m;
+    cin >> numCourses >> m;
+    int c1, c2;
+    int ch;
+    for (int i = 0; i < m; i++)
+    {
+        vector<int> aPrerequisite;
+        cin >> c1 >> c2;
+        aPrerequisite.push_back(c1);
+        aPrerequisite.push_back(c2);
+        prerequisites.push_back(aPrerequisite);
+    }
+
+    bool res = Solution().canFinish(numCourses, prerequisites);
+    cout << boolalpha << res << endl;
     return 0;
 }
