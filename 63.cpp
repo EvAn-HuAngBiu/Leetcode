f8835bc3f6c23bda660aa38d5994b992c60cc8ef
EvAn-HuAngBiu <17321168224@163.com>
2020-08-21 23:25:12 +0800

    Update question 63

--- 63.cpp
+++ 63.cpp
@@ -14,7 +14,7 @@ public:
         int n = obstacleGrid[0].size();
         vector<vector<long long>> dp(m, vector<long long>(n, 0));
         if (obstacleGrid[m - 1][n - 1] == 0) {
-            dp[m - 1][n - 1] = dp[m - 1][n - 1] = 1;
+            dp[m - 1][n - 1] = 1;
         }
         for (int i = m - 1; i >= 0; --i) {
             for (int j = n - 1; j >= 0; --j) {
