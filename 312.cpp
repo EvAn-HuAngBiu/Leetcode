2c24aa00ac6a294612764442cd37f9b4606d2981
EvAn-HuAngBiu <17321168224@163.com>
2020-08-22 09:09:37 +0800

    Update question 312

--- 312.cpp
+++ 312.cpp
@@ -4,70 +4,32 @@
 #include <vector>
 using namespace std;
 
-inline int max(int a, int b) {
-    return a > b ? a : b;
-}
-
-int sum = 0;
-void dfs(const vector<int>& nums, vector<bool>& visit, int remain, int curSum) {
-    int size = nums.size() - 2, s, j;
-    if (remain == 0) {
-        sum = max(sum, curSum);
-        return;
-    }
-    for (int i = 1; i <= size; i++) {
-        if (!visit[i]) {
-            visit[i] = true;
-            s = nums[i];
-            j = i - 1;
-            while (visit[j]) {
-                j--;
-            }
-            s *= nums[j];
-            j = i + 1;
-            while (visit[j]) {
-                j++;
-            }
-            s *= nums[j];
-            dfs(nums, visit, remain - 1, curSum + s);
-            visit[i] = false;
-        }
-    }
-}
-
-int maxCoins(vector<int>& nums) {
-    int size = nums.size();
-    vector<bool> visit(size + 2, false);
-    nums.emplace(nums.begin(), 1);
-    nums.emplace_back(1);
-    for (int i = 1; i <= size; i++) {
-        if (!visit[i]) {
-            visit[i] = true;
-            dfs(nums, visit, size - 1, nums[i - 1] * nums[i] * nums[i + 1]);
-            visit[i] = false;
-        }
-    }
-    return sum;
-}
-
-int maxCoins2(vector<int>& nums) {
-    int size = nums.size();
-    nums.emplace(nums.begin(), 1);
-    nums.emplace_back(1);
-    vector<vector<int>> dp(size + 2, vector<int>(size + 2, 0));
-    for (int j = 2; j <= size + 1; j++) {
-        for (int i = j - 2; i >= 0; i--) {
-            for (int k = i + 1; k < j; k++) {
-                dp[i][j] = max(dp[i][j], nums[i] * nums[k] * nums[j] + dp[i][k] + dp[k][j]);
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        nums.emplace_back(1);
+        nums.emplace(nums.begin(), 1);
+        int size = nums.size();
+        vector<vector<int>> dp(size, vector<int>(size));
+        for (int j = 2; j < size; ++j) {
+            for (int i = j - 2; i >= 0; --i) {
+                for (int k = i + 1; k < j; ++k) {
+                    dp[i][j] = max(dp[i][j], nums[i] * nums[j] * nums[k] + dp[i][k] + dp[k][j]);
+                }
             }
         }
+        return dp[0][size - 1];
     }
-    return dp[0][size + 1];
-}
+};
 
 int main()
 {
-    vector<int> nums = {35,16,83,87,84,59,48,41,20,54};
-    cout << maxCoins2(nums) << endl;
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+    cout << Solution().maxCoins(v) << endl;
     return 0;
 }
