d35d0cff2b12553d76f4ffc5eaa176e9a171a150
evan <17321168224@163.com>
2021-03-28 21:54:06 +0800

    Update Q326-Java

--- LeetCode-Java/src/base/Q326.java
+++ LeetCode-Java/src/base/Q326.java
@@ -9,12 +9,11 @@ package base;
  */
 public class Q326 {
     public boolean isPowerOfThree(int n) {
-        return Math.log(n) / Math.log(3) % 1 == 0;
+        return Math.log10(n) / Math.log10(3) % 1 == 0;
     }
 
     public static void main(String[] args) {
         Q326 q = new Q326();
-        System.out.println(q.isPowerOfThree(0));
-        System.out.println(Math.log(0));
+        System.out.println(q.isPowerOfThree(243));
     }
 }
