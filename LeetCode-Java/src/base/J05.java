a39823c10331a85d118af801811b29d1b54ecbea
evan <17321168224@163.com>
2021-04-07 21:57:58 +0800

    Update J05-Java

---
+++ LeetCode-Java/src/base/J05.java
@@ -0,0 +1,14 @@
+package base;
+
+/**
+ * Project: LeetCode-Java
+ * File: J05
+ *
+ * @author evan
+ * @date 2021/4/7
+ */
+public class J05 {
+    public String replaceSpace(String s) {
+        return s.replace(" ", "%20");
+    }
+}
