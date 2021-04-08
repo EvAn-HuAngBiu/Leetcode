7aa0e0cc3fed2c98166f4afb01ca37512c734171
evan <17321168224@163.com>
2021-04-08 16:49:47 +0800

    Update J09

--- LeetCode-Java/src/base/offer/J09.java
+++ LeetCode-Java/src/base/offer/J09.java
@@ -1,5 +1,37 @@
 package base.offer;
 
-public class J09 {
+import java.util.LinkedList;
+
+class CQueue {
+    private LinkedList<Integer> in;
+    private LinkedList<Integer> out;
+
+    public CQueue() {
+        in = new LinkedList<>();
+        out = new LinkedList<>();
+    }
     
+    public void appendTail(int value) {
+        in.push(value);
+    }
+    
+    public int deleteHead() {
+        if (out.isEmpty()) {
+            while (!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+        return out.isEmpty() ? -1 : out.pop();
+    }
+}
+
+public class J09 {
+    public static void main(String[] args) {
+        CQueue queue = new CQueue();
+        queue.appendTail(1);
+        queue.appendTail(2);
+        queue.appendTail(3);
+        queue.appendTail(4);
+        System.out.println(queue.deleteHead());
+    }
 }
