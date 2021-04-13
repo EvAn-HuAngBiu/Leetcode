9a794a1550fcd110b230a5d7fdff02d57c7cc2af
evan <17321168224@163.com>
2021-04-13 10:18:17 +0800

    Update J14

--- LeetCode-Java/src/base/offer/J14.java
+++ LeetCode-Java/src/base/offer/J14.java
@@ -1,14 +1,12 @@
 package base.offer;
 
 public class J14 {
-    private int[] dp;
-
     public int cuttingRope(int n) {
-        dp = new int[n + 1];
-        dp[2] = 1;
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = dp[2] = 1;
         for (int i = 3; i <= n; ++i) {
-            for (int j = 2; j < i; ++j) {
-                dp[i] = Math.max(dp[i], dp[j] * (i - j));
+            for (int j = 1; j < i; ++j) {
+                dp[i] = Math.max(dp[i], Math.max(dp[i - j] * j, j * (i - j)));
             }
         }
         return dp[n];
